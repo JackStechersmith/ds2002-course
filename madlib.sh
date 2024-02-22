feb5c76bbde5122a667e6dc7ffbd923e0a6181ea
Jack Stechersmith <jack@Jacks-MacBook-Pro.local>
2024-02-21 21:51:14 -0500

    Add the madlib file

---
+++ madlib.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+clear
+echo "Let's build a mad-lib!"
+
+read -p "1. Please give me an adjective: " ADJ1
+
+read -p "2. Please give me a noun: " NOU1
+
+read -p "3. Please give me a verb: " VER1
+
+read -p "4. Please give me an adjective: " ADJ2
+
+read -p "5. Please give me a noun: " NOU2
+
+read -p "6. Please give me a verb: " VER2
+
+read -p "7. Please give me an adjective: " ADJ3
+
+read -p "8. Please give me a noun: " NOU3
+clear
+
+echo "Once upon a time, there was a $ADJ1 $NOU1 that no one in the land would venture near."
+echo "One day, a charming knight decided that they would $VER1 far roads to seek a treasure."
+echo "A $ADJ2 witch laid eyes upon the knight's path. 'Come here, $NOU2' the witch whispered."
+echo "With one swift $VER2 the knight cut off the witch's head. The knight suddenly realized"
+echo "that the $ADJ1 $NOU1 was watching. The knight yelled 'I am not afraid of you!'"
+echo "A $ADJ3 king stepped out from the shadows and proclaimed '$NOU3!' and all was good in the world."
+
